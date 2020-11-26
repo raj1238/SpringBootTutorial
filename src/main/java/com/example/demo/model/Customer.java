b7c90357b191c7b233b2f9decc0d9b7d72958e85
raj1238 <shahrajyogesh@gmail.com>
2020-11-26 10:37:40 +0530

    Customer Class Created

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,4 +1,43 @@
 package com.example.demo.model;
 
+import org.springframework.stereotype.Component;
+
 public class Customer {
+
+    private int customerId;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
 }
