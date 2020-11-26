d11d81250374d61455e6c812914e3ae06a8220b8
raj1238 <shahrajyogesh@gmail.com>
2020-11-26 17:44:08 +0530

    Let's Start Fresh

--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -1,69 +1,15 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Customer;
-import org.springframework.stereotype.Component;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-@Component
-public class CustomerService {
+public interface CustomerService extends MongoRepository<Customer, String>  {
 
-    private int customerIdCount=1;
-    private List<Customer> customerList = new CopyOnWriteArrayList<>();
+    public Customer findByFirstName(String customerFirstName);
+    public List<Customer> findByLastName(String customerLastName);
 
-    public Customer addCustomer(Customer customer){
-
-        customer.setCustomerId(customerIdCount);
-        customerList.add(customer);
-        customerIdCount++;
-        return customer;
-    }
-
-    public List<Customer> getCustomers(){
-        return customerList;
-    }
-
-    public Customer getCustomer(int customerId){
-
-        return customerList
-                .stream()
-                .filter(c -> c.getCustomerId()==customerId)
-                .findFirst()
-                .get();
-
-    }
-
-    public Customer updateCustomer(int customerId, Customer customer){
-
-        customerList
-                .stream()
-                .forEach(c -> {
-                    if(c.getCustomerId()==customerId){
-                        c.setCustomerFirstName(customer.getCustomerFirstName());
-                        c.setCustomerLastName(customer.getCustomerLastName());
-                        c.setCustomerEmail(customer.getCustomerEmail());
-                    }
-                });
-
-        return customerList
-                .stream()
-                .filter(c-> c.getCustomerId()==customerId)
-                .findFirst()
-                .get();
-    }
-
-    public void deleteCustomer(int customerId){
-
-        customerList
-                .stream()
-                .forEach(c -> {
-                    if(c.getCustomerId()==customerId){
-                        customerList.remove(c);
-                    }
-                });
-
-    }
 }
 
 
